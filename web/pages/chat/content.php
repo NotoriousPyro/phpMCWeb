d4f45a58e7474918b1976d8bd4ad9de9bcdb4e87
Craig Crawford <craigcrawford1988@gmail.com>
2011-05-16 15:20:18 +0100

    dsfargeg

--- web/pages/chat/content.php
+++ web/pages/chat/content.php
@@ -20,15 +20,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style type="text/css">
-body,td,th {
+body,td,th 
+{
 	font-family: "Lucida Console", Monaco, monospace;
 	font-size: 11px;
 }
 </style>
 </head>
 
-<body>
-<?php
+<body><?php
 
 define("___ACCESS", TRUE);
 
@@ -47,6 +47,5 @@ foreach ($data as $message => $value)
 	echo "<div style=\"width: 75%; float: left;\">".$data[$message]["message"]."</div>\n";
 }
 
-?>
-</body>
+?></body>
 </html>
